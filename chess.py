cc23e1f84103ec5d83a83535ebab5ff026e93676
magnetenstad <tenstad.magne@gmail.com>
2020-07-18 03:58:52 +0200

    Structure code

--- chess.py
+++ chess.py
@@ -8,6 +8,19 @@ class Piece:
         self.type = type
         self.value = 0
 
+values = {
+    "P": 1,
+    "N": 3,
+    "B": 3,
+    "R": 5,
+    "Q": 10,
+    "K": 999
+}
+
+letters = ("a", "b", "c", "d", "e", "f", "g", "h")
+numbers = ("1", "2", "3", "4", "5", "6", "7", "8")
+turn = 1
+
 def board_create():
     board = [[-1 for i in range(8)] for i in range(8)]
     color = 0
@@ -198,59 +211,3 @@ def notpawn_get_moves(board, x, y, color, dx, dy, dlen, len, count = False):
                 else: moves.append((x1, y1))
             if tile != -1: break
     return moves
-
-values = {
-    "P": 1,
-    "N": 3,
-    "B": 3,
-    "R": 5,
-    "Q": 10,
-    "K": 999
-}
-
-board = board_create()
-
-board_print(board)
-
-letters = ("a", "b", "c", "d", "e", "f", "g", "h")
-numbers = ("1", "2", "3", "4", "5", "6", "7", "8")
-turn = 1
-
-while True:
-    while True:
-        try:
-            i = input("\n" + str((turn - 1) % 2) + " to play:").split()
-            if len(i) == 0:
-                break
-            if i[0] == "-":
-                turn -= 1
-            if i[0] == "exit":
-                break
-            a = (letters.index(i[0][0]), numbers.index(i[0][1]))
-            if len(i) == 1:
-                print(board[a[0]][a[1]].value)
-            b = (letters.index(i[1][0]), numbers.index(i[1][1]))
-            break
-        except:
-            pass
-    if len(i) == 0:
-        t = time.time()
-        eval = board_eval_recursive(board, 3, turn)
-        if eval != None:
-            tile_move(board, eval[0], eval[1])
-            print("Computer played:", letters[eval[0][0]] + numbers[eval[0][1]], letters[eval[1][0]] + numbers[eval[1][1]])
-            print("Eval: ", eval[2])
-            turn += 1
-        else:
-            print("No legal moves.")
-        print("Time:", time.time() - t)
-    else:
-        if i[0] == "exit":
-            break
-        moves = tile_get_moves(board, a[0], a[1])
-        if not b in moves:
-            print("Not a legal move.")
-        tile_move(board, a, b)
-        turn += 1
-    print("Current evaluation: ", board_eval(board), "\n")
-    board_print(board)
---
+++ chess_local.py
@@ -0,0 +1,49 @@
+from chess import *
+
+board = board_create()
+
+board_print(board)
+
+while True:
+    a = (0, 0)
+    b = (0, 0)
+    i = [""]
+
+    while True:
+        try:
+            i = input("\n" + str((turn - 1) % 2) + " to play:").split()
+            if len(i) == 0:
+                break
+            if i[0] == "-":
+                turn -= 1
+            if i[0] == "exit":
+                break
+            a = (letters.index(i[0][0]), numbers.index(i[0][1]))
+            if len(i) == 1:
+                print(board[a[0]][a[1]].value)
+            b = (letters.index(i[1][0]), numbers.index(i[1][1]))
+            break
+        except:
+            pass
+    if len(i) == 0:
+        t = time.time()
+        eval = board_eval_recursive(board, 3, turn)
+        if eval != None:
+            tile_move(board, eval[0], eval[1])
+            print("Computer played:", letters[eval[0][0]] +
+                  numbers[eval[0][1]], letters[eval[1][0]] + numbers[eval[1][1]])
+            print("Eval: ", eval[2])
+            turn += 1
+        else:
+            print("No legal moves.")
+        print("Time:", time.time() - t)
+    else:
+        if i[0] == "exit":
+            break
+        moves = tile_get_moves(board, a[0], a[1])
+        if not b in moves:
+            print("Not a legal move.")
+        tile_move(board, a, b)
+        turn += 1
+    print("Current evaluation: ", board_eval(board), "\n")
+    board_print(board)
