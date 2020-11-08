aabb7ffafc7f8b591936dad9d2c730c771e91b6a
magnetenstad <tenstad.magne@gmail.com>
2020-11-08 22:45:01 +0100

    Convert indentation to tabs

--- chess.py
+++ chess.py
@@ -3,27 +3,27 @@ import time
 import copy
 
 class Piece:
-    def __init__(self, color, type):
-        self.color = color
-        self.type = type
-        self.value = 0
+	def __init__(self, color, type):
+		self.color = color
+		self.type = type
+		self.value = 0
 
 values = {
-    "P": 1,
-    "N": 3,
-    "B": 3,
-    "R": 5,
-    "Q": 10,
-    "K": 999
+	"P": 1,
+	"N": 3,
+	"B": 3,
+	"R": 5,
+	"Q": 10,
+	"K": 999
 }
 
 icons = {
-    "P": ["♟", "♙"],
-    "N": ["♞", "♘"],
-    "B": ["♝", "♗"],
-    "R": ["♜", "♖"],
-    "Q": ["♛", "♕"],
-    "K": ["♚", "♔"]
+	"P": ["♟", "♙"],
+	"N": ["♞", "♘"],
+	"B": ["♝", "♗"],
+	"R": ["♜", "♖"],
+	"Q": ["♛", "♕"],
+	"K": ["♚", "♔"]
 }
 
 letters = ("a", "b", "c", "d", "e", "f", "g", "h")
@@ -31,192 +31,192 @@ numbers = ("1", "2", "3", "4", "5", "6", "7", "8")
 turn = 1
 
 def board_create():
-    board = [[-1 for i in range(8)] for i in range(8)]
-    color = 0
+	board = [[-1 for i in range(8)] for i in range(8)]
+	color = 0
 
-    for i in range(2):
-        board[0][i*7] = Piece(color, "R")
-        board[1][i*7] = Piece(color, "N")
-        board[2][i*7] = Piece(color, "B")
-        board[3][i*7] = Piece(color, "Q")
-        board[4][i*7] = Piece(color, "K")
-        board[5][i*7] = Piece(color, "B")
-        board[6][i*7] = Piece(color, "N")
-        board[7][i*7] = Piece(color, "R")
+	for i in range(2):
+		board[0][i*7] = Piece(color, "R")
+		board[1][i*7] = Piece(color, "N")
+		board[2][i*7] = Piece(color, "B")
+		board[3][i*7] = Piece(color, "Q")
+		board[4][i*7] = Piece(color, "K")
+		board[5][i*7] = Piece(color, "B")
+		board[6][i*7] = Piece(color, "N")
+		board[7][i*7] = Piece(color, "R")
 
-        for j in range(8):
-            board[j][1 + i*5] = Piece(color, "P")
+		for j in range(8):
+			board[j][1 + i*5] = Piece(color, "P")
 
-        color = 1
-    board_eval(board)
-    return board
+		color = 1
+	board_eval(board)
+	return board
 
 def board_eval(board):
-    value = 0
-    for x in range(len(board)):
-        for y in range(len(board[x])):
-            tile = board[x][y]
-            if tile != -1:
-                tile.value = tile_eval(board, x, y, tile)
-                value += tile.value
-    return value
+	value = 0
+	for x in range(len(board)):
+		for y in range(len(board[x])):
+			tile = board[x][y]
+			if tile != -1:
+				tile.value = tile_eval(board, x, y, tile)
+				value += tile.value
+	return value
 
 def board_print(board):
-    y = 7
-    while y >= 0:
-        row = str(y + 1) + " "
-        for x in range(len(board)):
-            tile = board[x][y]
-            if tile != -1: row += icons[tile.type][tile.color] + " "
-            else: row += "   "
-        y -= 1
-        print(row)
-    #print(" |_________________________")
-    print("  A B C D E F G H ")
+	y = 7
+	while y >= 0:
+		row = str(y + 1) + " "
+		for x in range(len(board)):
+			tile = board[x][y]
+			if tile != -1: row += icons[tile.type][tile.color] + " "
+			else: row += "   "
+		y -= 1
+		print(row)
+	#print(" |_________________________")
+	print("  A B C D E F G H ")
 
 def board_eval_move(board, a, b):
-    value = 0
-    dx = [-1, -1, 1, 1, -1, 1, 0, 0]
-    dy = [-1, 1, -1, 1, 0, 0, -1, 1]
+	value = 0
+	dx = [-1, -1, 1, 1, -1, 1, 0, 0]
+	dy = [-1, 1, -1, 1, 0, 0, -1, 1]
 
-    for t in (a, b):
-        for i in range(8):
-            for j in range(0, 8):
-                x = t[0] + dx[i] * j
-                y = t[1] + dy[i] * j
+	for t in (a, b):
+		for i in range(8):
+			for j in range(0, 8):
+				x = t[0] + dx[i] * j
+				y = t[1] + dy[i] * j
 
-                if x < 0 or 7 < x: break
-                if y < 0 or 7 < y: break
+				if x < 0 or 7 < x: break
+				if y < 0 or 7 < y: break
 
-                tile = board[x][y]
+				tile = board[x][y]
 
-                if tile != -1:
-                    tile.value = tile_eval(board, x, y, tile)
-                    break
+				if tile != -1:
+					tile.value = tile_eval(board, x, y, tile)
+					break
 
-    for x in range(8):
-        for y in range(8):
-            tile = board[x][y]
+	for x in range(8):
+		for y in range(8):
+			tile = board[x][y]
 
-            if tile != -1:
-                if tile.type == "N":
-                    tile.value = tile_eval(board, x, y, tile)
+			if tile != -1:
+				if tile.type == "N":
+					tile.value = tile_eval(board, x, y, tile)
 
-                value += tile.value
+				value += tile.value
 
-    return value
+	return value
 
 def board_eval_recursive(board, depth, turn):
-    depth -= 1
-    turn += 1
-    value = None
+	depth -= 1
+	turn += 1
+	value = None
 
-    for x in range(8):
-        for y in range(8):
-            tile = board[x][y]
+	for x in range(8):
+		for y in range(8):
+			tile = board[x][y]
 
-            if tile != -1 and tile.color == turn % 2:
-                moves = tile_get_moves(board, x, y)
+			if tile != -1 and tile.color == turn % 2:
+				moves = tile_get_moves(board, x, y)
 
-                for move in moves:
-                    board_new = copy.deepcopy(board)#[list(x) for x in board]
+				for move in moves:
+					board_new = copy.deepcopy(board)#[list(x) for x in board]
 
-                    tile_move(board_new, (x, y), move)
+					tile_move(board_new, (x, y), move)
 
-                    if depth > 0:
-                        _eval = board_eval_recursive(board_new, depth, turn)
-                        if _eval == None:
-                            _eval = ((x, y), move, board_eval_move(board, (x, y), move))
-                    else:
-                        _eval = ((x, y), move, board_eval_move(board, (x, y), move))#board_eval(board_new))
+					if depth > 0:
+						_eval = board_eval_recursive(board_new, depth, turn)
+						if _eval == None:
+							_eval = ((x, y), move, board_eval_move(board, (x, y), move))
+					else:
+						_eval = ((x, y), move, board_eval_move(board, (x, y), move))#board_eval(board_new))
 
-                    if _eval == None:
-                        continue
+					if _eval == None:
+						continue
 
-                    if value == None or (turn % 2 == 0 and _eval[2] > value[2]) or (turn % 2 == 1 and _eval[2] < value[2]):
-                        value = ((x, y), move, _eval[2])
+					if value == None or (turn % 2 == 0 and _eval[2] > value[2]) or (turn % 2 == 1 and _eval[2] < value[2]):
+						value = ((x, y), move, _eval[2])
 
-    return value
+	return value
 
 def tile_eval(board, x, y, tile):
-    value = values[tile.type]
-    value += (0.08 - 0.04 * (tile.type == "Q") - 0.08 * (tile.type == "K")) * tile_get_moves(board, x, y, count = True)
+	value = values[tile.type]
+	value += (0.08 - 0.04 * (tile.type == "Q") - 0.08 * (tile.type == "K")) * tile_get_moves(board, x, y, count = True)
 
-    if tile.type == "P" or tile.type == "N":
-        value -= 0.08 * (math.sqrt((x - 3.5)**2 + (y - 3.5)**2) - 2)
+	if tile.type == "P" or tile.type == "N":
+		value -= 0.08 * (math.sqrt((x - 3.5)**2 + (y - 3.5)**2) - 2)
 
-    if tile.type == "R":
-        value -= 0.1 * (y != 0 and y != 7)
+	if tile.type == "R":
+		value -= 0.1 * (y != 0 and y != 7)
 
-    return value * (1 - 2 * tile.color)
+	return value * (1 - 2 * tile.color)
 
 def tile_move(board, a, b):
-    board[b[0]][b[1]] = board[a[0]][a[1]]
-    board[a[0]][a[1]] = -1
+	board[b[0]][b[1]] = board[a[0]][a[1]]
+	board[a[0]][a[1]] = -1
 
 def tile_get_moves(board, x, y, count = False):
-    tile = board[x][y]
-    if count: moves = 0
-    else: moves = []
+	tile = board[x][y]
+	if count: moves = 0
+	else: moves = []
 
-    if tile != -1:
-        if tile.type == "P":
-            y1 = y + 1 - 2 * tile.color
+	if tile != -1:
+		if tile.type == "P":
+			y1 = y + 1 - 2 * tile.color
 
-            if y1 < 0 or 8 <= y1:
-                return moves
+			if y1 < 0 or 8 <= y1:
+				return moves
 
-            if board[x][y1] == -1:
-                if count: moves += 1
-                else: moves.append((x, y1))
+			if board[x][y1] == -1:
+				if count: moves += 1
+				else: moves.append((x, y1))
 
-                if tile.color == 0 and y == 1 and board[x][3] == -1:
-                    if count: moves += 1
-                    else: moves.append((x, 3))
+				if tile.color == 0 and y == 1 and board[x][3] == -1:
+					if count: moves += 1
+					else: moves.append((x, 3))
 
-                if tile.color == 1 and y == 6 and board[x][4] == -1:
-                    if count: moves += 1
-                    else: moves.append((x, 4))
+				if tile.color == 1 and y == 6 and board[x][4] == -1:
+					if count: moves += 1
+					else: moves.append((x, 4))
 
-            if 0 < x:
-                tile_l = board[x - 1][y1]
-                if tile_l != -1 and tile_l.color != tile.color:
-                    if count: moves += 1
-                    else: moves.append((x - 1, y1))
-            if x < 7:
-                tile_r = board[x + 1][y1]
-                if tile_r != -1 and tile_r.color != tile.color:
-                    if count: moves += 1
-                    else: moves.append((x + 1, y1))
-        
-        elif tile.type == "N":
-            moves = notpawn_get_moves(board, x, y, tile.color, [-1, 1, 2, 2, 1, -1, -2, -2], [-2, -2, -1, 1, 2, 2, 1, -1], 8, 1, count = count)
-        elif tile.type == "B":
-            moves = notpawn_get_moves(board, x, y, tile.color, [-1, -1, 1, 1], [-1, 1, -1, 1], 4, 7, count = count)
-        elif tile.type == "R":
-            moves = notpawn_get_moves(board, x, y, tile.color, [-1, 1, 0, 0], [0, 0, -1, 1], 4, 7, count = count)
-        elif tile.type == "Q":
-            moves = notpawn_get_moves(board, x, y, tile.color, [-1, -1, 1, 1, -1, 1, 0, 0], [-1, 1, -1, 1, 0, 0, -1, 1], 8, 7, count = count)
-        elif tile.type == "K":
-            moves = notpawn_get_moves(board, x, y, tile.color, [-1, -1, 1, 1, -1, 1, 0, 0], [-1, 1, -1, 1, 0, 0, -1, 1], 8, 1, count = count)
+			if 0 < x:
+				tile_l = board[x - 1][y1]
+				if tile_l != -1 and tile_l.color != tile.color:
+					if count: moves += 1
+					else: moves.append((x - 1, y1))
+			if x < 7:
+				tile_r = board[x + 1][y1]
+				if tile_r != -1 and tile_r.color != tile.color:
+					if count: moves += 1
+					else: moves.append((x + 1, y1))
+		
+		elif tile.type == "N":
+			moves = notpawn_get_moves(board, x, y, tile.color, [-1, 1, 2, 2, 1, -1, -2, -2], [-2, -2, -1, 1, 2, 2, 1, -1], 8, 1, count = count)
+		elif tile.type == "B":
+			moves = notpawn_get_moves(board, x, y, tile.color, [-1, -1, 1, 1], [-1, 1, -1, 1], 4, 7, count = count)
+		elif tile.type == "R":
+			moves = notpawn_get_moves(board, x, y, tile.color, [-1, 1, 0, 0], [0, 0, -1, 1], 4, 7, count = count)
+		elif tile.type == "Q":
+			moves = notpawn_get_moves(board, x, y, tile.color, [-1, -1, 1, 1, -1, 1, 0, 0], [-1, 1, -1, 1, 0, 0, -1, 1], 8, 7, count = count)
+		elif tile.type == "K":
+			moves = notpawn_get_moves(board, x, y, tile.color, [-1, -1, 1, 1, -1, 1, 0, 0], [-1, 1, -1, 1, 0, 0, -1, 1], 8, 1, count = count)
 
-    return moves
+	return moves
 
 def notpawn_get_moves(board, x, y, color, dx, dy, dlen, len, count = False):
-    if count: moves = 0
-    else: moves = []
-    for i in range(dlen):
-        for j in range(1, len + 1):
-            x1 = x + dx[i] * j
-            y1 = y + dy[i] * j
+	if count: moves = 0
+	else: moves = []
+	for i in range(dlen):
+		for j in range(1, len + 1):
+			x1 = x + dx[i] * j
+			y1 = y + dy[i] * j
 
-            if x1 < 0 or 8 <= x1: break
-            if y1 < 0 or 8 <= y1: break
+			if x1 < 0 or 8 <= x1: break
+			if y1 < 0 or 8 <= y1: break
 
-            tile = board[x1][y1]
+			tile = board[x1][y1]
 
-            if tile == -1 or tile.color != color:
-                if count: moves += 1
-                else: moves.append((x1, y1))
-            if tile != -1: break
-    return moves
+			if tile == -1 or tile.color != color:
+				if count: moves += 1
+				else: moves.append((x1, y1))
+			if tile != -1: break
+	return moves
--- chess_web.py
+++ chess_web.py
@@ -6,12 +6,12 @@ from chess import *
 from selenium import webdriver
 
 def driver_create():
-    if sys.platform == "win32": driver = webdriver.Chrome("lib/chromedriver.exe")
-    #if sys.platform == "darwin": driver = webdriver.Chrome("lib/chromedriver")
+	if sys.platform == "win32": driver = webdriver.Chrome("lib/chromedriver.exe")
+	#if sys.platform == "darwin": driver = webdriver.Chrome("lib/chromedriver")
 
-    driver.get("https://lichess.org/analysis")
+	driver.get("https://lichess.org/analysis")
 
-    return driver
+	return driver
 
 driver = driver_create()
 
@@ -22,75 +22,75 @@ board = board_create()
 board_print(board)
 
 while True:
-    a = (0, 0)
-    b = (0, 0)
-    i = [""]
-    
-    while True:
-        try:
-            i = input("\n" + str((turn - 1) % 2) + " to play:").split()
-            if len(i) == 0:
-                break
-            if i[0] == "-":
-                turn -= 1
-            if i[0] == "exit":
-                break
-            a = (letters.index(i[0][0]), numbers.index(i[0][1]))
-            if len(i) == 1:
-                print(board[a[0]][a[1]].value)
-            b = (letters.index(i[1][0]), numbers.index(i[1][1]))
-            break
-        except:
-            pass
+	a = (0, 0)
+	b = (0, 0)
+	i = [""]
+	
+	while True:
+		try:
+			i = input("\n" + str((turn - 1) % 2) + " to play:").split()
+			if len(i) == 0:
+				break
+			if i[0] == "-":
+				turn -= 1
+			if i[0] == "exit":
+				break
+			a = (letters.index(i[0][0]), numbers.index(i[0][1]))
+			if len(i) == 1:
+				print(board[a[0]][a[1]].value)
+			b = (letters.index(i[1][0]), numbers.index(i[1][1]))
+			break
+		except:
+			pass
 
-    if len(i) == 0:
-        t = time.time()
-        eval = board_eval_recursive(board, 3, turn)
+	if len(i) == 0:
+		t = time.time()
+		eval = board_eval_recursive(board, 3, turn)
 
-        if eval != None:
-            b = driver.find_element_by_class_name("cg-wrap")
+		if eval != None:
+			b = driver.find_element_by_class_name("cg-wrap")
 
-            x0 = eval[0][0]
-            y0 = eval[0][1]
-            x1 = eval[1][0]
-            y1 = eval[1][1]
+			x0 = eval[0][0]
+			y0 = eval[0][1]
+			x1 = eval[1][0]
+			y1 = eval[1][1]
 
-            orientation = b.get_attribute("class")
+			orientation = b.get_attribute("class")
 
-            if "orientation-black" in orientation:
-                x0 = 7 - x0
-                x1 = 7 - x1
-            else:
-                y0 = 7 - y0
-                y1 = 7 - y1
+			if "orientation-black" in orientation:
+				x0 = 7 - x0
+				x1 = 7 - x1
+			else:
+				y0 = 7 - y0
+				y1 = 7 - y1
 
-            action = webdriver.common.action_chains.ActionChains(driver)
-            action.move_to_element_with_offset(b, x0 * tile_size + tile_size/2, y0 * tile_size + tile_size/2)
-            action.click()
-            action.perform()
+			action = webdriver.common.action_chains.ActionChains(driver)
+			action.move_to_element_with_offset(b, x0 * tile_size + tile_size/2, y0 * tile_size + tile_size/2)
+			action.click()
+			action.perform()
 
-            action = webdriver.common.action_chains.ActionChains(driver)
-            action.move_to_element_with_offset(b, x1 * tile_size + tile_size/2, y1 * tile_size + tile_size/2)
-            action.click()
-            action.perform()
+			action = webdriver.common.action_chains.ActionChains(driver)
+			action.move_to_element_with_offset(b, x1 * tile_size + tile_size/2, y1 * tile_size + tile_size/2)
+			action.click()
+			action.perform()
 
-            tile_move(board, eval[0], eval[1])
-            print("Computer played:", letters[eval[0][0]] + numbers[eval[0][1]], letters[eval[1][0]] + numbers[eval[1][1]])
-            print("Eval: ", eval[2])
-            turn += 1
-        else:
-            print("No legal moves.")
+			tile_move(board, eval[0], eval[1])
+			print("Computer played:", letters[eval[0][0]] + numbers[eval[0][1]], letters[eval[1][0]] + numbers[eval[1][1]])
+			print("Eval: ", eval[2])
+			turn += 1
+		else:
+			print("No legal moves.")
 
-        print("Time:", time.time() - t)
-    else:
-        if i[0] == "exit":
-            break
-        moves = tile_get_moves(board, a[0], a[1])
-        if not b in moves:
-            print("Not a legal move.")
+		print("Time:", time.time() - t)
+	else:
+		if i[0] == "exit":
+			break
+		moves = tile_get_moves(board, a[0], a[1])
+		if not b in moves:
+			print("Not a legal move.")
 
-        tile_move(board, a, b)
-        turn += 1
+		tile_move(board, a, b)
+		turn += 1
 
-    print("Current evaluation: ", board_eval(board), "\n")
-    board_print(board)
+	print("Current evaluation: ", board_eval(board), "\n")
+	board_print(board)
